38ee629f6863b95fff24215e9432c9203b876074
Sam Parsons <samparsonsmusic@gmail.com>
2018-04-11 02:27:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function theBeatlesPlay(musicians, instruments) {
+  var list = [];
+  for (var i = 0; i < musicians.length; i++) {
+    var strng = `${musicians[i]} plays ${instruments[i]}`;
+    list.push(strng);
+  }
+  return list;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var ex = "!!!";
+  var newlist = [];
+  while (i < facts.length) {
+    newlist.push(`${facts[i]}` + '!!!');
+    i++;
+  }
+  return newlist;
+}
